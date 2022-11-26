20773d6c5d7cbdd3d9e16c207bd0204aba15fd46
David Brodd <117321132+Brodds0n@users.noreply.github.com>
2022-11-26 21:44:10 +0100

    Add files via upload

---
+++ input_file.js
@@ -0,0 +1,8 @@
+const prompt = require('prompt-sync')();
+
+var firstname = prompt("Enter your first name: ");
+var lastname =  prompt("Enter your last name: ");
+
+console.log("Your full name is",firstname,lastname);
+
+
