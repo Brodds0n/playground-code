035d3347ea044d5fdb1fa9f444ed708ebb2cf838
David Brodd <117321132+Brodds0n@users.noreply.github.com>
2022-12-15 20:16:58 +0100

    Add files via upload
    played around with Javascript a bit for educational purposes

---
+++ Experiment.js
@@ -0,0 +1,8 @@
+let userName = "David Brodd"
+console.log("Hello, " + userName + "!")
+
+let userAge = 20;
+console.log("You are " + userAge + " years old.");
+
+let isHappy = true;
+console.log("Are you happy? " + isHappy);
